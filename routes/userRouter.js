ae75f7379ec91094227c22ac234d87b133df5097
mohamed mohran <mohamed01098187312@gmail.com>
2025-07-19 05:59:55 +0300

    fix: cloudinary

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,20 @@
-const express = require("express"); 
+const express = require("express");
 
 const userController = require("./../controllers/userController");
 const authController = require("./../controllers/authController");
 
-const router = express.Router(); 
+const router = express.Router();
 
 const upload = require('../middleware/multer');
 const { uploadAvatar } = require('../controllers/userController');
 
-router.route("/get-all-users").get(userController.getAllUsers); 
-router.route("/update-password").patch(authController.protect,userController.updatePassword); 
-router.route("/update-me").patch(authController.protect,userController.updateMe); 
+router.route("/get-all-users").get(userController.getAllUsers);
+router.route("/update-password").patch(authController.protect, userController.updatePassword);
+router.route("/update-me").patch(authController.protect, userController.updateMe);
 
-router.post('/upload-avatar', upload.single('avatar'), uploadAvatar);
-
+router.post('/upload-avatar',
+    authController.protect,
+    upload.single('avatar'),
+    uploadAvatar
+);
 module.exports = router;
